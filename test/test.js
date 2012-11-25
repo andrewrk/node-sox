a1bd9a80ab5a0a2cd354d229961bf3df54ba6c2b
Andrew Kelley <superjoe30@gmail.com>
2012-11-24 22:45:52 -0500

    2 passing tests

--- test/test.js
+++ test/test.js
@@ -1,21 +1,36 @@
 var sox = require('../')
   , assert = require('assert')
   , path = require('path')
-  , soundWav = path.join(__dirname, 'sound.wav')
 
 describe("sox", function () {
-  it("identify", function(done) {
-    sox.identify(soundWav, function (err, results) {
-      if (err) return done(err);
-      assert.deepEqual(results, {
-        format: 'wav',
-        duration: 1.5,
-        sampleCount: 66150,
-        channelCount: 1,
-        bitRate: 722944,
-        sampleRate: 44100,
+  describe("identify", function () {
+    it("wav", function(done) {
+      sox.identify(path.join(__dirname, 'sound.wav'), function (err, results) {
+        if (err) return done(err);
+        assert.deepEqual(results, {
+          format: 'wav',
+          duration: 1.5,
+          sampleCount: 66150,
+          channelCount: 1,
+          bitRate: 722944,
+          sampleRate: 44100,
+        });
+        done();
       });
-      done();
+    });
+    it("mp3", function(done) {
+      sox.identify(path.join(__dirname, 'sound.mp3'), function (err, results) {
+        if (err) return done(err);
+        assert.deepEqual(results, {
+          format: 'mp3',
+          duration: 1.070998,
+          sampleCount: 47231,
+          channelCount: 1,
+          bitRate: 132096,
+          sampleRate: 44100,
+        });
+        done();
+      });
     });
   });
   it("transcode", function(done) {
